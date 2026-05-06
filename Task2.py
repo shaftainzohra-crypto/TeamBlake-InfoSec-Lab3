03db95b0e36cecda781b8ecb8b61e067c4506688
SaraGiuri <sara1906.giuriati@gmail.com>
2026-05-06 10:27:05 +0200

    Add support for multiple hash algorithms in MAC computation

--- Task2.py
+++ Task2.py
@@ -1,18 +1,33 @@
 import json
 from SHA256 import SHA256
+from SHA224 import SHA224
+from SHA384 import SHA384
+from SHA512 import SHA512
+from SHA512t import SHA512_224
+
 class MAC:
 
-    def mac_computation(key_hex,msg_hex):
+    def mac_computation(key_hex,msg_hex, hash):
         key_bytes = bytes.fromhex(key_hex)
         msg_bytes = bytes.fromhex(msg_hex)
-        return SHA256.hash_computation(key_bytes+msg_bytes)
+        if (hash == "256"):
+            return SHA256.hash_computation(key_bytes+msg_bytes)
+        if (hash == "224"):
+            return SHA224.hash_computation(key_bytes+msg_bytes)
+        if (hash == "384"):
+            return SHA384.hash_computation(key_bytes+msg_bytes)
+        if (hash == "512"):
+            return SHA512.hash_computation(key_bytes+msg_bytes)
+        if (hash == "512t"):
+            return SHA512_224.hash_computation(key_bytes+msg_bytes)
+
 
 #test
 with open("lab3task2.json", "r") as f:
     tests = json.load(f)
 #print(MAC.mac_computation("80000000000000000000000000000000","73686f7274"))
 for test in tests:
-    result = MAC.mac_computation(test["key"], test["msg"])
+    result = MAC.mac_computation(test["key"], test["msg"],"256")
     expected = test["tag"]
 
     status = "PASS" if result == expected else "FAIL"
